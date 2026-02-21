143dbd29ffcad7d8114f5ae122125968c5ffeb9f
Maicon Leles <maiconnpml@gmail.com>
2026-02-21 12:06:51 -0300

    refactor(ytmusic): Move toPlaylist to response_types.go
    The musicTowRowItem.toPlaylist method it's better located in the same
    file wich it's receiver is defined.

--- pkg/goytmusic/playlists.go
+++ pkg/goytmusic/playlists.go
@@ -15,31 +15,7 @@ type Playlist struct {
 	BrowseID       string
 	Author         *string
 	AuthorBrowseID *string
-}
-
-// toPlaylist parses a musicTwoRowItem struct to a Playlist
-func (mt *musicTowRowItem) toPlaylist() *Playlist {
-	pl := &Playlist{}
-	if len(mt.MusicTwoRow.Title.Runs) > 0 {
-		pl.Name = mt.MusicTwoRow.Title.Runs[0].Text
-	}
-	if len(mt.MusicTwoRow.Subtitle.Runs) > 0 {
-		runs := mt.MusicTwoRow.Subtitle.Runs
-		pl.Author = &runs[0].Text
-
-		if runs[0].NavEndpoint != nil && runs[0].NavEndpoint.BrowseEndpoint != nil {
-			pl.AuthorBrowseID = runs[0].NavEndpoint.BrowseEndpoint.BrowseID
-		}
-	}
-
-	if mt.MusicTwoRow.Endpoint != nil && mt.MusicTwoRow.Endpoint.BrowseEndpoint != nil {
-		pl.BrowseID = *mt.MusicTwoRow.Endpoint.BrowseEndpoint.BrowseID
-	}
-
-	return pl
-}
-
-	}
+	Tracks         []*Track
 }
 
 // ListLiked retrieves and returns an array of Playlist. This array
