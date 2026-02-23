997c41ae652c97983c8a773aa74408876bb3f7cd
Maicon Leles <maiconnpml@gmail.com>
2026-02-22 21:17:56 -0300

    test(ytmusic): Create tests for playlist requests
    As the InnerTube API responses can be changed at any time, this commit
    creates tests with golden files to verify response consistence between
    test runs.

---
+++ pkg/goytmusic/playlists_test.go
@@ -0,0 +1,83 @@
+package goytmusic
+
+import (
+	"flag"
+	"os"
+	"testing"
+)
+
+// Define a flag -update. Use: go test -update
+var update = flag.Bool("update", false, "update json files with API data")
+
+func TestListLikedPlaylistsExtraction(t *testing.T) {
+	const filePath = "testdata/liked_playlists.json"
+
+	if *update {
+		cookie := os.Getenv("AUTH_COOKIE")
+		if cookie == "" {
+			t.Fatal("AUTH_COOKIE not configured. Impossible to update testdata.")
+		}
+
+		client := NewClient(nil).WithAuthCookie(cookie)
+
+		req, _ := client.NewRequest("POST", "browse", client.BrowseBody(brIDLikedPlaylists))
+		body, _, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("Error while retrieving liked playlists: %v", err)
+		}
+
+		err = os.WriteFile(filePath, body, 0o644)
+		if err != nil {
+			t.Fatalf("Error while writing golden file: %v", err)
+		}
+		t.Logf("File %s successfully updated!", filePath)
+	}
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		t.Fatalf("Test file not found. Run with -update: %v", err)
+	}
+
+	// TODO: improve verification to find exactly were the change occurred
+	playlists := extractPlaylists(data)
+	if len(playlists) == 0 {
+		t.Error("No playlists found.")
+	}
+}
+
+func TestGetPlaylistExtraction(t *testing.T) {
+	const filePath = "testdata/playlist_tracks.json"
+	const playlistBrowseID = "VLPLrG2h7_c1yh1tgTYhJlrXvgEIgGfL96BV" // public playlist
+
+	if *update {
+		cookie := os.Getenv("AUTH_COOKIE")
+		if cookie == "" {
+			t.Fatal("AUTH_COOKIE not configured. Impossible to update testdata.")
+		}
+
+		client := NewClient(nil).WithAuthCookie(cookie)
+
+		req, _ := client.NewRequest("POST", "browse", client.BrowseBody(playlistBrowseID))
+		body, _, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("Error while retrieving playlist: %v", err)
+		}
+
+		err = os.WriteFile(filePath, body, 0o644)
+		if err != nil {
+			t.Fatalf("Error while writing golden file: %v", err)
+		}
+		t.Logf("File %s successfully updated!", filePath)
+	}
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		t.Fatalf("Test file not found. Run with -update: %v", err)
+	}
+
+	// TODO: improve verification to find exactly were the change occurred
+	playlist := extractPlaylistWithTracks(data)
+	if len(playlist.Tracks) == 0 {
+		t.Error("No track extracted from playlist.")
+	}
+}
